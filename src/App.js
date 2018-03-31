98e7159dff560606c6dd5391c01cbd014115a131
mohamedrafi <fair7993@gmail.com>
2018-03-31 09:40:42 +0530

    basic layout added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Icon from './icons'
+const electron = window.require("electron")
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.windowMinimize = this.windowMinimize.bind(this)
+    this.windowMaximize = this.windowMaximize.bind(this)
+    this.windowClose    = this.windowClose.bind(this)
+  }
+
+  windowMinimize(){
+    electron.remote.BrowserWindow.getFocusedWindow().minimize()
+  }
+  windowMaximize(){
+    if(electron.remote.BrowserWindow.getFocusedWindow().isMaximized()) {
+      electron.remote.BrowserWindow.getFocusedWindow().unmaximize()
+    } else {
+      electron.remote.BrowserWindow.getFocusedWindow().maximize()
+    }
+  }
+  windowClose(){
+    electron.remote.BrowserWindow.getFocusedWindow().close()
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <div className='header'>
+          <span onClick={this.windowMinimize}><Icon icon='minimize' iconClass='header-icon'></Icon></span>
+          <span onClick={this.windowMaximize}><Icon icon='restore' iconClass='header-icon'></Icon></span>
+          <span onClick={this.windowClose}><Icon icon='close' iconClass='header-icon'></Icon></span>
+        </div>
       </div>
     );
   }
